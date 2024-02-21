7dd8ef4add89ea65c9ca30b695d1e2c6c86d976e
DarkAtra <darkatra@gmail.com>
2024-02-21 19:52:42 +0100

    chore: use netty as webserver

--- src/main/kotlin/de/darkatra/Application.kt
+++ src/main/kotlin/de/darkatra/Application.kt
@@ -2,10 +2,10 @@ package de.darkatra
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
-import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.html.respondHtml
 import io.ktor.server.http.content.staticResources
+import io.ktor.server.netty.Netty
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
@@ -25,8 +25,8 @@ fun Application.module() {
 fun main() {
 
     val server = embeddedServer(
-        factory = CIO,
-        host = "localhost",
+        factory = Netty,
+        host = "0.0.0.0",
         port = 8080,
         module = Application::module
     )
